e23e483ffb1750ed06c7574c8c650091a61e8a1a
Kazeken <Kazeken@DESKTOP-HA9K0NH>
2021-05-29 21:23:36 +0600

    add Apstate

---
+++ ChatBlazorApp/Client/AppState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatBlazorApp.Client
+{
+    public class AppState
+    {
+        public string UserName { get; set; }
+        public List<string> Rooms { get; set; } = new List<string>();
+    }
+}
--- ChatBlazorApp/Client/Program.cs
+++ ChatBlazorApp/Client/Program.cs
@@ -19,6 +19,8 @@ namespace ChatBlazorApp.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddSingleton<AppState>();
+
             await builder.Build().RunAsync();
         }
     }
