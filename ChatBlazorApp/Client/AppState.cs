5744c1963ec98c30a09342cf0f582d05bccc2ae8
kazeken <81683480+kazeken@users.noreply.github.com>
2021-06-08 17:28:28 +0600

    Room Notifications part 1

--- ChatBlazorApp/Client/AppState.cs
+++ ChatBlazorApp/Client/AppState.cs
@@ -9,7 +9,7 @@ namespace ChatBlazorApp.Client
     public class AppState
     {
         public string UserName { get; set; }
-        public List<string> Rooms { get; set; } = new List<string>();
+        public List<(string RoomName, int NotificationCount)> Rooms { get; set; } = new List<(string, int)>();
         public string RoomsCommaSeparated { get; set; }
         public Action AppStateUpdated { get; set; }
         public Room CurrentRoom { get; set; }
